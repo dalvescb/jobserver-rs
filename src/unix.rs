7299f56b99411dc3bc8c09069f3bbc9afe17c759
Curtis D'Alves <dalvescb@mac.home>
2025-11-22 00:43:09 -0500

    fix pthread_t cast breaking zos

--- src/unix.rs
+++ src/unix.rs
@@ -389,7 +389,7 @@ impl Helper {
                 // return an error, but on other platforms it may not. In
                 // that sense we don't actually know if this will succeed or
                 // not!
-                libc::pthread_kill(self.thread.as_pthread_t() as _, libc::SIGUSR1);
+                libc::pthread_kill(self.thread.as_pthread_t() as libc::pthread_t, libc::SIGUSR1);
             }
             state = self
                 .state
